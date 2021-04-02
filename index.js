6fc4048519aacef181385cf5d641ad9037d554e4
fardinkhan69 <fardinofficial69@gmail.com>
2021-04-02 18:50:56 +0600

    done all

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
  
   const productCollection = client.db("dhakarDokan").collection("products");
+  const orderCollection = client.db("dhakarDokan").collection("orders")
 
   app.get('/allProducts',(req, res)=>{
     productCollection.find()
@@ -38,14 +39,40 @@ client.connect(err => {
   })
 
 
+  app.get('/orderedProducts',(req, res)=>{
+    console.log(req.query.email)
+    orderCollection.find({email:req.query.email})
+    .toArray((err,items)=>{
+      res.send(items)
+    })
+  })
+
   
 
-
+  app.get('/cheackout/:id',(req, res)=>{
+    console.log(req.params.id);
+    productCollection.find({_id:ObjectId(req.params.id)})
+    .toArray((err,documents)=>{
+      res.send(documents[0])
+    })
+    
+  })
 
   app.post('/addProduct',(req,res)=>{
     const product = req.body;
     console.log("new product",product);
     productCollection.insertOne(product)
+    .then(result => {
+      console.log(result);
+      res.send(result.insertedCount > 0)
+    })
+    
+  })
+
+  app.post('/orderdone',(req,res)=>{
+    const orderDetail = req.body;
+    console.log("new product",orderDetail);
+    orderCollection.insertOne(orderDetail)
     .then(result => {
       console.log(result);
       res.send(result.insertedCount > 0)
